4ff98d18720f8c9997c067c48cd0c727c950ed4b
Aleyna Naz Aydogan <aydoganaleynanaz@gmail.com>
2026-01-20 19:22:36 +0300

    Koordinat gorunumu sadelestirildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ChessApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ChessApp extends StatelessWidget {
+  const ChessApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: Scaffold(
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(title: const Text('Knight\'s Path')),
+        body: const Center(child: ChessBoard()),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ChessBoard extends StatelessWidget {
+  const ChessBoard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    // ekran boyutunu al
+
+    final size = MediaQuery.of(context).size;
+    final double boardWidth = size.width < size.height
+        ? size.width
+        : size.height;
+
+    return Container(
+      width: boardWidth * 0.9,
+      height: boardWidth * 0.9,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white, width: 2),
+        boxShadow: const [
+          BoxShadow(color: Colors.black54, blurRadius: 8, offset: Offset(2, 2)),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 64,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 8,
         ),
+        itemBuilder: (context, index) {
+          int x = index ~/ 8;
+          int y = index % 8;
+          bool isLight = (x + y) % 2 == 0;
+          List<String> columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+          // taslari yerlestir
+          Widget? tasIcon;
+
+          // beyazlar
+          if (x == 7 && y == 4) {
+            tasIcon = SvgPicture.asset('assets/images/beyaz_sah.svg');
+          } else if (x == 7 && y == 6) {
+            tasIcon = SvgPicture.asset('assets/images/beyaz_at.svg');
+          }
+          // siyahlar
+          else if (x == 0 && y == 4) {
+            tasIcon = SvgPicture.asset('assets/images/siyah_sah.svg');
+          } else if (x == 0 && y == 6) {
+            tasIcon = SvgPicture.asset('assets/images/siyah_at.svg');
+          }
+
+          // kenar koordinatlari
+
+          Widget? sayiText;
+          if (y == 0) {
+            sayiText = Positioned(
+              left: 2,
+              top: 2,
+              child: Text(
+                "${8 - x}",
+                style: TextStyle(
+                  color: isLight ? Colors.brown : Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 10,
+                ),
+              ),
+            );
+          }
+
+          Widget? harfText;
+          if (x == 7) {
+            harfText = Positioned(
+              right: 2,
+              bottom: 2,
+              child: Text(
+                columns[y],
+                style: TextStyle(
+                  color: isLight ? Colors.brown : Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 10,
+                ),
+              ),
+            );
+          }
+
+          // 3. kare
+          return Container(
+            color: isLight ? const Color(0xFFF0D9B5) : const Color(0xFFB58863),
+            child: Stack(
+              children: [
+                // Koordinatlari ekle (sadece kenarlardaysa gorunur)
+                if (sayiText != null) sayiText,
+                if (harfText != null) harfText,
+
+                // Tasi ekle (eger varsa)
+                if (tasIcon != null)
+                  Center(
+                    child: SizedBox(
+                      width: size.width * 0.08,
+                      height: size.width * 0.08,
+                      child: tasIcon,
+                    ),
+                  ),
+              ],
+            ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
